051a11a1d3a47a4635e09ee63f3d6f565d12f5aa
Angel Figuera <angeleraser@gmail.com>
2022-10-21 15:12:17 -0400

    style: add board corners styles

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 const boardEl = document.getElementById("board");
+const BOARD_SIZE = 3;
 
-function createBoardBtn(rowId = 0, colId = 0) {
+let gameBoard = [],
+  isX = true,
+  isEnded = false;
+
+boardEl.addEventListener("click", (e) => {
+  if (isEnded) return e.preventDefault();
+
+  const { target: btn } = e;
+  const { rowid, colid } = btn.dataset;
+  const key = isX ? "X" : "O";
+
+  if (!btn.dataset.key) {
+    btn.textContent = key;
+    btn.dataset.key = key;
+    gameBoard[rowid][colid] = key;
+    isX = !isX;
+  }
+
+  isEnded = hasWinner(gameBoard, [Number(rowid), Number(colid)], key);
+});
+
+initGameBoard();
+
+function createBoardBtn(rowId = 0, colId = 0, boardSize) {
   const btn = document.createElement("button");
+  const isCorner = colId === 0 || colId === boardSize - 1;
+
+  const classes = classNames({
+    "btn-corner-bottom": rowId === boardSize - 1,
+    "btn-corner-left": colId === 0,
+    "btn-corner-right": colId === boardSize - 1,
+    "btn-corner-top": rowId === 0,
+    "btn-corner": isCorner,
+  });
+
   btn.classList.add("board-btn");
+  classes.length && btn.classList.add(...classes);
   btn.dataset.rowid = rowId;
   btn.dataset.colid = colId;
   btn.dataset.key = "";
+
   return btn;
 }
 
@@ -25,7 +61,7 @@ function getBoardTemplate(size = 0) {
 
 function renderBoardHTML(size = 0) {
   const buttons = getBoardTemplate(size).map((r, ri) =>
-    r.map((c, ci) => createBoardBtn(ri, ci))
+    r.map((c, ci) => createBoardBtn(ri, ci, size))
   );
   boardEl.style.gridTemplateColumns = "1fr ".repeat(buttons.length);
   boardEl.innerHTML = "";
@@ -64,31 +100,12 @@ function initGameBoard() {
   gameBoard = getBoardTemplate(BOARD_SIZE);
 }
 
-const BOARD_SIZE = 7;
+function classNames(deps) {
+  const classes = Object.entries(deps).map((item, i) => {
+    const [key, value] = item;
+    if (!value || !key) return "";
+    return key;
+  });
 
-let gameBoard = [],
-  isX = true,
-  isEnded = false;
-
-boardEl.addEventListener("click", (e) => {
-  if (isEnded) return e.preventDefault();
-
-  const { target: btn } = e;
-  const { rowid, colid } = btn.dataset;
-  const key = isX ? "X" : "O";
-
-  if (!btn.dataset.key) {
-    btn.textContent = key;
-    btn.dataset.key = key;
-    gameBoard[rowid][colid] = key;
-    isX = !isX;
-  }
-
-  if (hasWinner(gameBoard, [Number(rowid), Number(colid)], key)) {
-    isEnded = true;
-    alert(`${key} WINS`);
-    initGameBoard();
-  }
-});
-
-initGameBoard();
+  return classes.length ? classes.filter((v) => v) : "";
+}
