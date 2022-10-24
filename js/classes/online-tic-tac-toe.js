58c1df11e391087e83507b43f29d4cda426c1fe1
Angel Figuera <angeleraser@gmail.com>
2022-10-24 14:29:56 -0400

    refactor: improve app layout and socket events listening

--- js/classes/online-tic-tac-toe.js
+++ js/classes/online-tic-tac-toe.js
@@ -13,83 +13,85 @@ const EVENTS = {
 class OnlineTicTacToe extends TicTacToe {
   constructor(options) {
     super(options);
-    this.socket = io("http://localhost:3000/");
+    this.socket = io ? io("http://localhost:3000/") : null;
     this.id = this.socket.id;
     this.isRemoteOnline = false;
   }
 
   init() {
-    this.allowBoardWriting = false;
+    localStorage.clear();
+    if (this.socket === null) return;
 
+    this.allowBoardWriting = false;
     this.render();
 
-    this.onBoardClick(({ cellValue, coords }) => {
-      if (!this.allowBoardWriting) return;
+    this.socket.on("connect", () => {
+      this.onBoardClick(({ cellValue, coords }) => {
+        if (!this.allowBoardWriting) return;
+
+        this.socket.emit(
+          EVENTS.BOARD_CLICK,
+          this.getEventPayload({ cellValue, coords })
+        );
+
+        this.writeBoardCell(cellValue, coords);
+
+        if (!this.getCurrentPlayer()) {
+          localStorage.setItem("CURRENT_PLAYER", cellValue);
+        }
+
+        this.allowBoardWriting = false;
+      });
 
       this.socket.emit(
-        EVENTS.BOARD_CLICK,
-        this.getEventPayload({ cellValue, coords })
+        "join-room",
+        this.getEventPayload({ roomid: this.roomid })
       );
 
-      this.writeBoardCell(cellValue, coords);
+      this.socket.on(EVENTS.BOARD_CLICK, (payload) => {
+        const { cellValue, coords } = payload;
 
-      if (!this.getCurrentPlayer()) {
-        localStorage.setItem("CURRENT_PLAYER", cellValue);
-      }
+        this.writeBoardCell(cellValue, coords);
+        this.allowBoardWriting = true;
+      });
 
+      this.socket.on(EVENTS.TWO_PLAYERS_JOIN, ({ socketId }) => {
+        this.allowBoardWriting = true;
+        this.isRemoteOnline = true;
+
+        console.log(`Player ${socketId} joined the room.`);
+
+        this.socket.emit(
+          EVENTS.RECOVER_STATE,
+          this.getEventPayload({ state: this.state })
+        );
+      });
+
+      this.socket.on(EVENTS.PLAYER_DISCONNECT, ({ socketId }) => {
+        this.allowBoardWriting = false;
+        this.isRemoteOnline = false;
+
+        console.warn(`Player ${socketId} left the room.`);
+
+        setTimeout(() => {
+          if (!this.isRemoteOnline) this.socket.disconnect();
+        }, 30000);
+      });
+
+      this.socket.on(EVENTS.RECOVER_STATE, (payload) => {
+        this.setState(payload.state);
+        this.allowBoardWriting = true;
+        this.renderHTML(payload);
+      });
+
+      this.socket.on(EVENTS.UNABLE_JOIN, () => {
+        alert("Unable to join room: " + this.roomid);
+      });
+    });
+
+    this.socket.on("disconnect", () => {
       this.allowBoardWriting = false;
-    });
-
-    this.socket.emit(
-      "join-room",
-      this.getEventPayload({ roomid: this.roomid })
-    );
-
-    this.socket.on(EVENTS.BOARD_CLICK, (payload) => {
-      const { cellValue, coords } = payload;
-
-      this.writeBoardCell(cellValue, coords);
-      this.allowBoardWriting = true;
-    });
-
-    this.socket.on(EVENTS.TWO_PLAYERS_JOIN, ({ socketId }) => {
-      this.allowBoardWriting = true;
-      this.isRemoteOnline = true;
-
-      console.log(`Player ${socketId} joined the room.`);
-
-      this.socket.emit(
-        EVENTS.RECOVER_STATE,
-        this.getEventPayload({ state: this.state })
-      );
-    });
-
-    this.socket.on(EVENTS.PLAYER_DISCONNECT, ({ socketId }) => {
-      this.allowBoardWriting = false;
-      this.isRemoteOnline = false;
-
-      console.warn(`Player ${socketId} left the room.`);
-
-      // alert(
-      //   "The other party has left the room \nYou will be disconnected in 30 sec if the player does not join..."
-      // );
-
-      setTimeout(() => {
-        if (!this.isRemoteOnline) this.socket.disconnect();
-      }, 30000);
-    });
-
-    this.socket.on(EVENTS.RECOVER_STATE, (payload) => {
-      this.setState(payload.state);
-
-      this.allowBoardWriting =
-        this.getNextPlayerKey() === localStorage.getItem("CURRENT_PLAYER");
-
-      setTimeout(() => this.renderHTML(payload), 0);
-    });
-
-    this.socket.on(EVENTS.UNABLE_JOIN, () => {
-      alert("Unable to join room: " + this.roomid);
+      console.warn("You has been disconnected from server.");
     });
   }
 
